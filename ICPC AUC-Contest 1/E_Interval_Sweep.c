2bc3a9098938ec19ee1013e97e6aa77d2cb2cdf9
MD Raihan Hosen <raihanhosen1381@gmail.com>
2026-04-26 12:03:42 +0600

    checks Interval

---
+++ ICPC AUC-Contest 1/E_Interval_Sweep.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a == 0 && b == 0) {
+        printf("NO\n");
+    } 
+    else if (abs(a - b) <= 1) {
+        printf("YES\n");
+    } 
+    else {
+        printf("NO\n");
+    }
+
+    return 0;
+}
